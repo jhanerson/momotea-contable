f23e582a8953f8a1ce63cba49e7de26864dcad2c
Mohacking <96668230+jhanerson@users.noreply.github.com>
2023-09-22 13:57:25 -0500

    V-1-001

--- site/code.js
+++ site/code.js
@@ -4,41 +4,81 @@ window.onload = function () {
     $('body').removeClass('no-scroll')
 }
 
-// Variables para llevar un registro de la suma total
-let sumaTotal = 0;
+// Espera a que el documento HTML esté completamente cargado
+document.addEventListener("DOMContentLoaded", function () {
+    // Obtén referencias a los elementos del formulario
+    const ventaForm = document.getElementById("ventaForm");
+    const productoInput = document.getElementById("producto");
+    const cantidadInput = document.getElementById("cantidad");
+    const precioInput = document.getElementById("precio");
+    const ivaCheckbox = document.getElementById("flexCheckDefault");
+    const ventasTableBody = document.getElementById("ventasTableBody");
 
-// Agrega un evento de envío al formulario
-ventaForm.addEventListener('submit', function (event) {
-    event.preventDefault(); // Evita que se envíe el formulario
+    // Array para almacenar las ventas
+    const ventas = [];
 
-    // Obtiene los valores de los campos de entrada
-    const producto = document.getElementById('producto').value;
-    const cantidad = parseFloat(document.getElementById('cantidad').value); // Convierte a número
-    const precio = parseFloat(document.getElementById('precio').value); // Convierte a número
+    // Función para calcular el total de la venta
+    function calcularTotal(precio, cantidad) {
+        const subtotal = precio * cantidad;
+        const ivaPorcentaje = ivaCheckbox.checked ? 0.19 : 0;
+        const iva = subtotal * ivaPorcentaje;
+        const total = subtotal + iva;
+        return total.toFixed(2); // Redondear a 2 decimales
+    }
 
-    // Calcula el total para esta fila
-    const total = cantidad * precio;
+    // Función para agregar una venta a la tabla
+    function agregarVenta(producto, cantidad, precio, total) {
+        const fecha = new Date().toLocaleDateString();
+        const newRow = document.createElement("tr");
+        newRow.innerHTML = `
+            <td>${producto}</td>
+            <td>${cantidad}</td>
+            <td>${precio}</td>
+            <td>${total}</td>
+            <td>${fecha}</td>
+        `;
+        ventasTableBody.appendChild(newRow);
+    }
 
-    // Actualiza la suma total
-    sumaTotal += total;
+    // Manejador de evento para el envío del formulario
+    ventaForm.addEventListener("submit", function (e) {
+        e.preventDefault(); // Evita la recarga de la página
 
-    // Crea una nueva fila en la tabla con los valores ingresados y el total
-    const newRow = document.createElement('tr');
-    newRow.innerHTML = `
-    <td>${producto}</td>
-    <td>${cantidad}</td>
-    <td>${precio}</td>
-    <td>${total}</td>
-    <td>${new Date().toLocaleDateString()}</td>
-    <td>${sumaTotal}</td>`; // Muestra la suma total
+        // Obtén los valores ingresados por el usuario
+        const producto = productoInput.value;
+        const cantidad = parseFloat(cantidadInput.value);
+        const precio = parseFloat(precioInput.value);
 
+        // Validación básica
+        if (isNaN(cantidad) || isNaN(precio) || producto.trim() === "") {
+            alert("Por favor, complete todos los campos correctamente.");
+            return;
+        }
 
+        // Calcula el total de la venta
+        const totalVenta = calcularTotal(precio, cantidad);
 
-    // Agrega la nueva fila a la tabla
-    ventasTableBody.appendChild(newRow);
+        // Agrega la venta al array
+        ventas.push({
+            producto,
+            cantidad,
+            precio,
+            total: totalVenta,
+        });
 
-    // Resetea los valores de los campos de entrada
-    document.getElementById('producto').value = '';
-    document.getElementById('cantidad').value = '';
-    document.getElementById('precio').value = '';
+        // Agrega la venta a la tabla
+        agregarVenta(producto, cantidad, precio, totalVenta);
+
+        // Limpia los campos del formulario
+        productoInput.value = "";
+        cantidadInput.value = "";
+        precioInput.value = "";
+
+        // Muestra un mensaje de confirmación
+        alert("Venta registrada con éxito.");
+    });
 });
+
+
+
+
